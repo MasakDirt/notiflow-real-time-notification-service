f1f7d299f8911c332dc47049b3c60dc5b98cf275
maksym_korniev <maksimkarulet8@gmail.com>
2024-01-11 00:12:46 +0200

    created RoleRepository interface with method findByName

---
+++ user/src/main/java/com/proj/user/repository/RoleRepository.java
@@ -0,0 +1,13 @@
+package com.proj.user.repository;
+
+import com.proj.user.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+
+    Optional<Role> findByName(String name);
+}
