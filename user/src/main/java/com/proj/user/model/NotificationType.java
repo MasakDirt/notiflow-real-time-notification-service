b9776508f375f5a862471118600e874b802868e3
maksym_korniev <maksimkarulet8@gmail.com>
2024-01-10 03:48:38 +0200

    created NotificationType enum class

---
+++ user/src/main/java/com/proj/user/model/NotificationType.java
@@ -0,0 +1,20 @@
+package com.proj.user.model;
+
+public enum NotificationType {
+    TELEGRAM("Telegram"), EMAIL("E-mail");
+
+    private final String name;
+
+    NotificationType(String name) {
+        this.name = name;
+    }
+
+    public static NotificationType getTypeFromName(String name) {
+        for (NotificationType type : NotificationType.values()) {
+            if (type.name.equals(name)) {
+                return type;
+            }
+        }
+        throw new EnumConstantNotPresentException(NotificationType.class, name);
+    }
+}
