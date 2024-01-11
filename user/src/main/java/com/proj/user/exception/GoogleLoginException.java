50fe2b628bac64ce8ae57bb2cf3c2090f491ffc7
maksym_korniev <maksimkarulet8@gmail.com>
2024-01-12 00:29:28 +0200

    created GoogleLoginException which throwing during logging via oauth2

---
+++ user/src/main/java/com/proj/user/exception/GoogleLoginException.java
@@ -0,0 +1,8 @@
+package com.proj.user.exception;
+
+public class GoogleLoginException extends RuntimeException{
+
+    public GoogleLoginException(String message) {
+        super(message);
+    }
+}
