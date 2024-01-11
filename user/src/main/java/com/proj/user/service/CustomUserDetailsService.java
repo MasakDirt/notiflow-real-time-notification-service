16dbee6eadd6e3f71652832acbc0bf5304db7cb7
maksym_korniev <maksimkarulet8@gmail.com>
2024-01-12 00:32:39 +0200

    created CustomUserDetailsService which implements - UserDetailsService and has method loadUserByUsername

---
+++ user/src/main/java/com/proj/user/service/CustomUserDetailsService.java
@@ -0,0 +1,23 @@
+package com.proj.user.service;
+
+import com.proj.user.model.User;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@AllArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserService userService;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userService.readByEmail(username);
+        log.info("Get {Local} user by username - {}", username);
+        return user;
+    }
+}
