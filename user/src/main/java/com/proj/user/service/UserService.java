59c010bd6ff8f43c62b05573d09a07dc62da249c
maksym_korniev <maksimkarulet8@gmail.com>
2024-01-12 00:38:07 +0200

    added method for creating user which authorize with google and addded some methods for checking if it is this user in UserService class

--- user/src/main/java/com/proj/user/service/UserService.java
+++ user/src/main/java/com/proj/user/service/UserService.java
@@ -1,5 +1,8 @@
 package com.proj.user.service;
 
+import com.proj.user.model.CustomOAuth2User;
+import com.proj.user.model.NotificationType;
+import com.proj.user.model.Provider;
 import com.proj.user.model.User;
 import com.proj.user.repository.UserRepository;
 import jakarta.persistence.EntityNotFoundException;
@@ -8,6 +11,9 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+
+import java.util.Objects;
+import java.util.Optional;
 
 @Service
 @AllArgsConstructor
@@ -20,8 +26,33 @@ public class UserService {
     public User create(User user, String roleName) {
         user.setPassword(passwordEncoder.encode(user.getPassword()));
         user.setRole(roleService.readByName(roleName));
+        if (!isUserHasGoogleProvider(user)) {
+            user.setProvider(Provider.LOCAL);
+        }
 
         return userRepository.saveAndFlush(user);
+    }
+
+    private boolean isUserHasGoogleProvider(User user) {
+        return Objects.nonNull(user.getProvider()) &&
+                user.getProvider().equals(Provider.GOOGLE);
+    }
+
+    public User processOAuthPostLogin(CustomOAuth2User customOAuth2User) {
+        Optional<User> existUser = userRepository.findByEmail(customOAuth2User.getName());
+
+        return existUser.orElseGet(() -> createNewUserFromOAuth2(customOAuth2User));
+    }
+
+    private User createNewUserFromOAuth2(CustomOAuth2User customOAuth2User) {
+        User newUser = new User();
+        newUser.setEmail(customOAuth2User.getName());
+        newUser.setFullName(customOAuth2User.getFullName());
+        newUser.setPassword(customOAuth2User.getAttributes().get("sub").toString());
+        newUser.setProvider(Provider.GOOGLE);
+        newUser.setNotificationType(NotificationType.EMAIL);
+
+        return create(newUser, "USER");
     }
 
     public User readById(long id) {
