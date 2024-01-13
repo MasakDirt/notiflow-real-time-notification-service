73db1df3358a18edc9b22d5f75282f8a86707eed
maksym_korniev <maksimkarulet8@gmail.com>
2024-01-13 02:02:27 +0200

    created new dto- UserResponse

---
+++ user/src/main/java/com/proj/user/dto/UserResponse.java
@@ -0,0 +1,38 @@
+package com.proj.user.dto;
+
+import com.proj.user.model.Provider;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+
+@Data
+@Builder
+@Getter
+public class UserResponse {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @NotEmpty(message = "Fill in the field with the name.")
+    private String fullName;
+
+    @Pattern(regexp = "[\\w-\\.]+@([\\w-]+\\.)+[\\w-]{2,4}", message = "Must be a valid e-mail address")
+    private String email;
+
+    @NotBlank(message = "The password cannot be 'blank'")
+    private String password;
+
+    @Pattern(regexp = "^@.*", message = "The telegram account must started with '@' character")
+    private String telegram;
+
+    @NotNull
+    private String notificationType;
+
+    private Provider provider;
+}
