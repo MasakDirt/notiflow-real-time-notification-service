3a1278147e701c794d028a667662dfeb2e856ace
maksym_korniev <maksimkarulet8@gmail.com>
2024-01-15 23:42:18 +0200

    RoleService refactor methods for better reading and added logging

--- user/src/main/java/com/proj/user/service/RoleService.java
+++ user/src/main/java/com/proj/user/service/RoleService.java
@@ -4,8 +4,10 @@ import com.proj.user.model.Role;
 import com.proj.user.repository.RoleRepository;
 import jakarta.persistence.EntityNotFoundException;
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 @AllArgsConstructor
 public class RoleService {
@@ -13,14 +15,23 @@ public class RoleService {
     private final RoleRepository roleRepository;
 
     public Role create(String name) {
+        checkValidRoleName(name);
+        Role created = roleRepository.save(Role.of(name));
+        log.info("Role with name {} successfully created", name);
+        return created;
+    }
+
+    private void checkValidRoleName(String name) {
         if (name == null || name.trim().isEmpty()) {
+            log.error("This role name is empty or null - {}", name);
             throw new NullPointerException("The name of Role cannot be empty!");
         }
-        return roleRepository.save(Role.of(name));
     }
 
     public Role readByName(String name) {
-        return roleRepository.findByName(name)
+        Role role = roleRepository.findByName(name)
                 .orElseThrow(() -> new EntityNotFoundException("Role not found!"));
+        log.info("Read role with name {}", name);
+        return role;
     }
 }
