b07c815bde51a67f7c197fa19cc0fccf9362a123
maksym_korniev <maksimkarulet8@gmail.com>
2024-01-15 23:46:57 +0200

    created new Service for checking is Users same - AuthUserService

---
+++ user/src/main/java/com/proj/user/service/AuthUserService.java
@@ -0,0 +1,14 @@
+package com.proj.user.service;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@AllArgsConstructor
+public class AuthUserService {
+    private final UserService userService;
+
+    public boolean isUserSame(long id, String email) {
+        return userService.readByEmail(email).getId() == id;
+    }
+}
