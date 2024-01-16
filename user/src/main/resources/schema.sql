b21ea211a646fea386303879d27ee078fa01c461
maksym_korniev <maksimkarulet8@gmail.com>
2024-01-16 22:14:16 +0200

    schema.sql added new param to users table - age

--- user/src/main/resources/schema.sql
+++ user/src/main/resources/schema.sql
@@ -8,6 +8,7 @@ create table users (id bigserial not null, role_id bigint, "e-mail" varchar(255)
                     full_name varchar(255) not null,notification_type varchar(255)
                         not null check (notification_type in ('TELEGRAM','EMAIL')),
                     password varchar(255) not null, provider varchar(255)
-                        check (provider in ('LOCAL','GOOGLE')), telegram varchar(255), primary key (id));
+                        check (provider in ('LOCAL','GOOGLE')), telegram varchar(255), age integer, primary key (id));
 
+alter table if exists users alter column age set data type integer;
 alter table if exists users add constraint FKp56c1712k691lhsyewcssf40f foreign key (role_id) references roles;
