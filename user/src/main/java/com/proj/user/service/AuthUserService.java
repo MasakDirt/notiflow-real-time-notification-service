5590cb713627d2a3704fcdf36836fd940c02777d
maksym_korniev <maksimkarulet8@gmail.com>
2024-01-16 22:11:56 +0200

    AuthUserService added new method - isAdmin for checking by email if user is admin

--- user/src/main/java/com/proj/user/service/AuthUserService.java
+++ user/src/main/java/com/proj/user/service/AuthUserService.java
@@ -6,9 +6,17 @@ import org.springframework.stereotype.Service;
 @Service
 @AllArgsConstructor
 public class AuthUserService {
+
     private final UserService userService;
 
     public boolean isUserSame(long id, String email) {
         return userService.readByEmail(email).getId() == id;
+    }
+
+    public boolean isAdmin(String email) {
+        return userService.readByEmail(email)
+                .getRole()
+                .getName()
+                .equals("ADMIN");
     }
 }
