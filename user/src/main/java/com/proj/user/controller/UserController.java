07fc920fbb859f9b29a23a602e4ca1cdbd20dd36
maksym_korniev <maksimkarulet8@gmail.com>
2024-01-17 01:59:32 +0200

    UserController added requestparams to method getUsers also refactor this method to new view model and added mapping user to update request in method getUpdateForm

--- user/src/main/java/com/proj/user/controller/UserController.java
+++ user/src/main/java/com/proj/user/controller/UserController.java
@@ -3,12 +3,14 @@ package com.proj.user.controller;
 import com.proj.user.config.RedirectConfig;
 import com.proj.user.dto.UpdateRequest;
 import com.proj.user.mapper.UserMapper;
+import com.proj.user.service.AuthUserService;
 import com.proj.user.service.UserService;
 import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
 import org.springframework.ui.ModelMap;
@@ -16,6 +18,7 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.time.LocalDateTime;
+import java.util.Arrays;
 
 @Slf4j
 @RestController
@@ -25,25 +28,46 @@ public class UserController {
 
     private final UserService userService;
     private final UserMapper userMapper;
+    private final AuthUserService authUserService;
 
     @GetMapping
-    public ModelAndView getUsers(ModelMap modelMap, Authentication authentication) {
-        modelMap.addAttribute("users", userService.getAll(Pageable.ofSize(4)));
-        log.info("USERS-GET === {}", authentication.getName());
-        return new ModelAndView("users", modelMap);
+    public ModelAndView getUsers(
+            @RequestParam(name = "page", required = false, defaultValue = "0") int pageNumber,
+            @RequestParam(name = "sort_order", defaultValue = "desc") String sortOrder,
+            @RequestParam(name = "sort_by", defaultValue = "id") String[] sortBy,
+            ModelMap modelMap, Authentication authentication) {
+        String authUserEmail = authentication.getName();
+        modelMap.addAttribute("email", authUserEmail);
+        modelMap.addAttribute("isAdmin", authUserService.isAdmin(authUserEmail));
+        modelMap.addAttribute("page", pageNumber);
+        modelMap.addAttribute("sort_order", sortOrder);
+        modelMap.addAttribute("sort_by", getSortByValues(sortBy));
+        modelMap.addAttribute("users", userService.getAll(
+                        PageRequest.of(pageNumber, 3, Sort.by(Sort.Direction.fromString(sortOrder), sortBy))
+                )
+        );
+        log.info("USERS-GET === {}", authUserEmail);
+        return new ModelAndView("users-list", modelMap);
     }
 
-    @GetMapping("/update/{id}")
-    @PreAuthorize("@authUserService.isUserSame(#id, authentication.principal)")
+    private String getSortByValues(String[] sortBy) {
+        String sortByAsString = Arrays.toString(sortBy);
+        return sortByAsString.substring(1, sortByAsString.length() - 1);
+    }
+
+    @GetMapping("/{id}/update")
+    @PreAuthorize("@authUserService.isUserAdminOrSame(#id, authentication.name)")
     public ModelAndView getUpdateForm(@PathVariable long id, ModelMap modelMap, Authentication authentication) {
-        modelMap.addAttribute("updateRequest", UpdateRequest.withId(id));
+        var user = userService.readById(id);
+        modelMap.addAttribute("updateRequest", userMapper.getUpdateRequestFromUser(user));
+
         log.info("UPDATE-FORM-GET === {}, time = {}", authentication.getPrincipal(), LocalDateTime.now());
 
         return new ModelAndView("user-update", modelMap);
     }
 
-    @PostMapping("/update/{id}")
-    @PreAuthorize("@authUserService.isUserSame(#id, authentication.principal)")
+    @PostMapping("/{id}/update")
+    @PreAuthorize("@authUserService.isUserAdminOrSame(#id, authentication.name)")
     public void update(@PathVariable long id, @Valid UpdateRequest updateRequest,
                        HttpServletResponse response, Authentication authentication) {
         userService.update(userMapper.getUserFromUpdateRequest(updateRequest));
