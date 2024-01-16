f38e235760f4c5c4920df3b146314f21da4e971a
maksym_korniev <maksimkarulet8@gmail.com>
2024-01-16 22:14:51 +0200

    data.sql added new parameter to users creation provider and age

--- user/src/main/resources/data.sql
+++ user/src/main/resources/data.sql
@@ -1,5 +1,5 @@
 INSERT INTO roles (name) VALUES ('ADMIN'), ('USER');
 
-INSERT INTO users(notification_type, role_id, "e-mail", full_name, password, telegram)
+INSERT INTO users(notification_type, role_id, "e-mail", full_name, password, provider, telegram, age)
 VALUES ('TELEGRAM', 1, 'admin@mail.co', 'Admin Korniev',
-        '$2a$10$bIjGIiFFRtxMLutKDvuPTeTtpROhRceXImnIqzOUkkwN/aBMJE/hu', '@mskdrttt');
+        '$2a$10$bIjGIiFFRtxMLutKDvuPTeTtpROhRceXImnIqzOUkkwN/aBMJE/hu', 'LOCAL', '@mskdrttt', 19);
