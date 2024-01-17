e003c488db14a7ad0a83a3fd589e20d579a82dec
maksym_korniev <maksimkarulet8@gmail.com>
2024-01-18 00:27:11 +0200

    AuthUserService created new method for checking is it google user

--- user/src/main/java/com/proj/user/service/AuthUserService.java
+++ user/src/main/java/com/proj/user/service/AuthUserService.java
@@ -1,5 +1,6 @@
 package com.proj.user.service;
 
+import com.proj.user.model.Provider;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -11,6 +12,10 @@ public class AuthUserService {
 
     public boolean isUserAdminOrSame(long id, String email) {
         return isAdmin(email) || userService.readByEmail(email).getId() == id;
+    }
+
+    public boolean isUserAuthWithGoogle(long id) {
+        return userService.readById(id).getProvider().equals(Provider.GOOGLE);
     }
 
     public boolean isAdmin(String email) {
