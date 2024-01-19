cf0b639a73d1340feb17e69b78a63d0e281a69bd
maksym_korniev <maksimkarulet8@gmail.com>
2024-01-19 22:27:59 +0200

    created new exception class - NullRoleNameException

---
+++ user/src/main/java/com/proj/user/exception/NullRoleNameException.java
@@ -0,0 +1,8 @@
+package com.proj.user.exception;
+
+public class NullRoleNameException extends RuntimeException{
+
+    public NullRoleNameException(String message) {
+        super(message);
+    }
+}
