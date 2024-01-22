c55c32549d01c35993c2cfec01aa07a01de88881
maksym_korniev <maksimkarulet8@gmail.com>
2024-01-22 22:50:31 +0200

    rename class from NullRoleNameException to RoleNameException

--- user/src/main/java/com/proj/user/exception/NullRoleNameException.java
+++
@@ -1,8 +0,0 @@
-package com.proj.user.exception;
-
-public class NullRoleNameException extends RuntimeException{
-
-    public NullRoleNameException(String message) {
-        super(message);
-    }
-}
