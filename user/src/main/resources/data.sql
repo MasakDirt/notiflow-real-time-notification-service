93aaf2a9b5903e0075fcda39e2727ae0233aab87
maksym_korniev <maksimkarulet8@gmail.com>
2024-01-23 22:36:58 +0200

    added to file data.sql new inserting of user

--- user/src/main/resources/data.sql
+++ user/src/main/resources/data.sql
@@ -3,3 +3,7 @@ INSERT INTO roles (name) VALUES ('ADMIN'), ('USER');
 INSERT INTO users(notification_type, role_id, "e-mail", full_name, password, provider, telegram, age)
 VALUES ('TELEGRAM', 1, 'admin@mail.co', 'Admin Korniev',
         '$2a$10$bIjGIiFFRtxMLutKDvuPTeTtpROhRceXImnIqzOUkkwN/aBMJE/hu', 'LOCAL', '@mskdrttt', 19);
+
+INSERT INTO users(notification_type, role_id, "e-mail", full_name, password, provider, telegram, age)
+VALUES ('EMAIL', 2, 'user@mail.co', 'User Korniev',
+        '$2a$10$5VGVN5FUUnZKKoaMhGZiBeYoQcut0ZqRQz.L8AZFNrb3iFKYMwCFW', 'GOOGLE', '@userstelegram', 20);
