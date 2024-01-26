b9f8129bb5e3d7db7e4d1790830d4289c680fa1b
maksym_korniev <maksimkarulet8@gmail.com>
2024-01-26 23:58:51 +0200

    added telegram main app class

---
+++ telegram-kafka/src/main/java/com/proj/telegramkafka/TelegramKafkaApplication.java
@@ -0,0 +1,12 @@
+package com.proj.telegramkafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelegramKafkaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TelegramKafkaApplication.class, args);
+    }
+}
