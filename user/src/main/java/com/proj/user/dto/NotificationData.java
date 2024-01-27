b8e07b986d1c50f3b6d3e24a1c7bc7d45f91b28c
maksym_korniev <maksimkarulet8@gmail.com>
2024-01-28 01:26:57 +0200

    users NotificationData class added new private constructor with setting - recipientUserTelegram and senderUserName fields, and use it in new static method "forTelegram"

--- user/src/main/java/com/proj/user/dto/NotificationData.java
+++ user/src/main/java/com/proj/user/dto/NotificationData.java
@@ -21,4 +21,24 @@ public class NotificationData {
 
     @NotEmpty(message = "Message must not be empty!")
     private String message;
+
+    private NotificationData(String recipientUserTelegram, String senderUserName) {
+        this.recipientUserTelegram = recipientUserTelegram;
+        this.senderUserName = senderUserName;
+        this.message = String.format("""
+                Hello %s,
+                              
+                You have successfully subscribed to receive notifications from %s.\s
+                You will now receive updates and notifications whenever %s posts or shares new content.
+                              
+                If you wish to unsubscribe at any time, simply click on the "Unsubscribe" button in your settings.
+                              
+                Thank you for using our notification service!
+                              
+                Notiflow""", recipientUserTelegram, senderUserName, senderUserName);
+    }
+
+    public static NotificationData forTelegram(String recipientUserTelegram, String senderUserName) {
+        return new NotificationData(recipientUserTelegram, senderUserName);
+    }
 }
