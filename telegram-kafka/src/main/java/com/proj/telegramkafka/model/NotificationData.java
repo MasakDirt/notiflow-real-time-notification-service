27505d1e37798047ed7cf5385adb1e1d50e6e641
maksym_korniev <maksimkarulet8@gmail.com>
2024-01-27 18:30:46 +0200

    created NotificationData class for selecting users data for notifications

---
+++ telegram-kafka/src/main/java/com/proj/telegramkafka/model/NotificationData.java
@@ -0,0 +1,33 @@
+package com.proj.telegramkafka.model;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import lombok.*;
+
+@Builder
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode
+@ToString
+public class NotificationData {
+
+    @NotNull(message = "The telegram account must started with '@' character")
+    @Pattern(regexp = "^@.+", message = "The telegram account must started with '@' character")
+    private String recipientUserTelegram;
+
+    private String senderUserName;
+
+    @NotEmpty(message = "Message must not be empty!")
+    private String message;
+
+    public static NotificationData fromJsonString(String jsonData) {
+        try {
+            return new ObjectMapper().readValue(jsonData, NotificationData.class);
+        } catch (Exception e) {
+            throw new RuntimeException("JsonData isn`t appropriate to notification data");
+        }
+    }
+}
