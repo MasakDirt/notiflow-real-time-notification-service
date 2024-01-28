cb6ba1d213f91aa6c0b9a0816feea9a89e2b63e0
maksym_korniev <maksimkarulet8@gmail.com>
2024-01-29 00:21:54 +0200

    created new model class - TelegramUser

---
+++ telegram-kafka/src/main/java/com/proj/telegramkafka/model/TelegramUser.java
@@ -0,0 +1,25 @@
+package com.proj.telegramkafka.model;
+
+import lombok.*;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+
+@Entity
+@Getter
+@Setter
+@EqualsAndHashCode
+@ToString
+@Table(name = "telegram_users")
+public class TelegramUser {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @NotEmpty(message = "The username cannot be empty")
+    private String username;
+
+    @NotEmpty(message = "The chatId cannot be empty")
+    @Column(name = "chat_id", nullable = false)
+    private String chatId;
+}
