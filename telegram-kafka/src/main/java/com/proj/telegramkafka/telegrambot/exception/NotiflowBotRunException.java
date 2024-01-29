ffd4df0595dab8087fcb6035dd2fb20343f5a0f0
maksym_korniev <maksimkarulet8@gmail.com>
2024-01-29 20:22:23 +0200

    created new class exception - NotiflowBotRunException

---
+++ telegram-kafka/src/main/java/com/proj/telegramkafka/telegrambot/exception/NotiflowBotRunException.java
@@ -0,0 +1,7 @@
+package com.proj.telegramkafka.telegrambot.exception;
+
+public class NotiflowBotRunException extends RuntimeException{
+    public NotiflowBotRunException(String message) {
+        super(message);
+    }
+}
