7ed6b8c6213f4ededda53dc5891cb35a5ec23f1e
maksym_korniev <maksimkarulet8@gmail.com>
2024-01-30 00:35:48 +0200

    user module NotificationData remove unused field - senderUserName, refactor of setting message and message at all

--- user/src/main/java/com/proj/user/dto/NotificationData.java
+++ user/src/main/java/com/proj/user/dto/NotificationData.java
@@ -3,6 +3,8 @@ package com.proj.user.dto;
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
+
+import com.proj.user.model.User;
 import lombok.*;
 
 @Builder
@@ -17,28 +19,25 @@ public class NotificationData {
     @Pattern(regexp = "^@.+", message = "The telegram account must started with '@' character")
     private String recipientUserTelegram;
 
-    private String senderUserName;
-
     @NotEmpty(message = "Message must not be empty!")
     private String message;
 
-    private NotificationData(String recipientUserTelegram, String senderUserName) {
+    private NotificationData(String recipientUserTelegram) {
         this.recipientUserTelegram = recipientUserTelegram;
-        this.senderUserName = senderUserName;
-        this.message = String.format("""
-                Hello %s,
-                              
-                You have successfully subscribed to receive notifications from %s.\s
-                You will now receive updates and notifications whenever %s posts or shares new content.
-                              
-                If you wish to unsubscribe at any time, simply click on the "Unsubscribe" button in your settings.
-                              
-                Thank you for using our notification service!
-                              
-                Notiflow""", recipientUserTelegram, senderUserName, senderUserName);
     }
 
-    public static NotificationData forTelegram(String recipientUserTelegram, String senderUserName) {
-        return new NotificationData(recipientUserTelegram, senderUserName);
+    public static NotificationData forTelegram(User recipient, User sender) {
+        NotificationData notificationData = new NotificationData(recipient.getTelegram());
+        notificationData.message = String.format("""
+                Hello "%s"(%s)👋,
+                              
+                You have successfully subscribed to receive notifications from "%s"(%s).\s
+                You will now receive updates and notifications whenever %s posts or shares new content.
+                              
+                Thank you for using our notification service❤️!
+                              
+                Notiflow🤖""", recipient.getFullName(), recipient.getTelegram(),
+                sender.getFullName(), sender.getTelegram(), sender.getTelegram());
+        return notificationData;
     }
 }
