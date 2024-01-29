0035488c70620fa7cb877e9b192d0d56d550c456
maksym_korniev <maksimkarulet8@gmail.com>
2024-01-29 20:23:57 +0200

    added to KafkaListeners constructor and fields - TelegramUserService, NotiflowBot, and use notiflowBot method - sendMsg

--- telegram-kafka/src/main/java/com/proj/telegramkafka/kafka/listeners/KafkaListeners.java
+++ telegram-kafka/src/main/java/com/proj/telegramkafka/kafka/listeners/KafkaListeners.java
@@ -1,13 +1,24 @@
 package com.proj.telegramkafka.kafka.listeners;
 
 import com.proj.telegramkafka.model.NotificationData;
+import com.proj.telegramkafka.service.TelegramUserService;
+import com.proj.telegramkafka.telegrambot.NotiflowBot;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
 @Slf4j
 @Component
 public class KafkaListeners {
+    private final TelegramUserService telegramUserService;
+    private final NotiflowBot notiflowBot;
+
+    @Autowired
+    public KafkaListeners(String token, TelegramUserService telegramUserService) {
+        this.telegramUserService = telegramUserService;
+        this.notiflowBot = new NotiflowBot(token, telegramUserService);
+    }
 
     @KafkaListener(
             topics = "telegram",
@@ -16,5 +27,6 @@ public class KafkaListeners {
     void listener(String jsonData) {
         NotificationData notificationData = NotificationData.fromJsonString(jsonData);
         log.info("Listener received: {}", notificationData);
+        notiflowBot.sendMsg(telegramUserService.readByUsername(notificationData.getRecipientUserTelegram()).getChatId(), notificationData.getMessage());
     }
 }
