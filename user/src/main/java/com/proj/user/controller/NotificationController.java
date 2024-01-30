2c91fc5543c9fecda264fd07a482f92be81d7bb3
maksym_korniev <maksimkarulet8@gmail.com>
2024-01-30 23:25:04 +0200

    created new Controller for notifications with methods - getNotificationFromUser and getSuccessPage

---
+++ user/src/main/java/com/proj/user/controller/NotificationController.java
@@ -0,0 +1,42 @@
+package com.proj.user.controller;
+
+import com.proj.user.config.RedirectConfig;
+import com.proj.user.service.UserService;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.Authentication;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletResponse;
+
+@Slf4j
+@RestController
+@AllArgsConstructor
+@RequestMapping("/api/v1/get-notification")
+public class NotificationController {
+
+    private final UserService userService;
+
+    @PostMapping("/{sender-id}")
+    public void getNotificationFromUser(@PathVariable("sender-id") long senderId,
+                                        Authentication authentication, HttpServletResponse response) {
+        String recipientEmail = authentication.getName();
+        log.info("Preparing to receive data to user {}", recipientEmail);
+        userService.sendDataToTelegramNotification(recipientEmail, senderId);
+        log.info("Data successfully transfer!");
+        RedirectConfig.redirect("/api/v1/get-notification/success", response);
+    }
+
+    @GetMapping("/success")
+    public ModelAndView getSuccessPage(ModelMap modelMap, Authentication authentication) {
+        var authUser = userService.readByEmail(authentication.getName());
+        var authNotificationType = authUser.getNotificationType();
+        String authUserName = authUser.getFullName();
+
+        modelMap.addAttribute("notificationType", authNotificationType.getName());
+        modelMap.addAttribute("userName", authUserName);
+        return new ModelAndView("success-notification");
+    }
+}
