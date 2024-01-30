9639ccadf40811cfdea19c8d01e7dfb95899bc60
maksym_korniev <maksimkarulet8@gmail.com>
2024-01-30 23:34:10 +0200

    created new model - ErrorResponse

---
+++ telegram-kafka/src/main/java/com/proj/telegramkafka/model/ErrorResponse.java
@@ -0,0 +1,33 @@
+package com.proj.telegramkafka.model;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.datatype.jsr310.deser.LocalDateDeserializer;
+import lombok.Builder;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.http.HttpStatus;
+
+import javax.validation.constraints.NotNull;
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@Getter
+@Setter
+public class ErrorResponse {
+
+    @JsonDeserialize(using = LocalDateDeserializer.class)
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime timestamp;
+
+    @NotNull
+    private HttpStatus status;
+
+    @NotNull
+    private String message;
+
+    @NotNull
+    private String path;
+}
