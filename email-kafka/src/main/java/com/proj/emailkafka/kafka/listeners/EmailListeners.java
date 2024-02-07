206c8558c7af95def34b7ab4a462baecaed115c8
maksym_korniev <maksimkarulet8@gmail.com>
2024-02-07 20:16:59 +0200

    EmailListeners added new field RestTemplate and new private method tryToSendEmailNotification

--- email-kafka/src/main/java/com/proj/emailkafka/kafka/listeners/EmailListeners.java
+++ email-kafka/src/main/java/com/proj/emailkafka/kafka/listeners/EmailListeners.java
@@ -1,11 +1,12 @@
 package com.proj.emailkafka.kafka.listeners;
 
-import com.proj.emailkafka.EmailSender;
+import com.proj.emailkafka.email.EmailSender;
 import com.proj.emailkafka.model.NotificationData;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
 @Slf4j
 @Component
@@ -13,6 +14,7 @@ import org.springframework.stereotype.Component;
 public class EmailListeners {
 
     private final EmailSender emailSender;
+    private final RestTemplate restTemplate;
 
     @KafkaListener(
             topics = "email",
@@ -21,6 +23,15 @@ public class EmailListeners {
     void listener(String jsonData) {
         NotificationData notificationData = NotificationData.fromJsonString(jsonData);
         log.info("Listener received: {}", notificationData);
-        emailSender.sendEmailNotification(notificationData);
+        tryToSendEmailNotification(notificationData);
+    }
+
+    private void tryToSendEmailNotification(NotificationData notificationData) {
+        try {
+            emailSender.sendEmail(notificationData);
+        } catch (Exception exception) {
+            log.error("Exception while sending email - {}", exception.getMessage());
+            restTemplate.postForLocation("http://USER/api/v1/get-notification/if-success?", false);
+        }
     }
 }
